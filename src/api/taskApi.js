87198dfba480817bff02d9d71c351df7e73fe686
jihan-p <jihan.pahlevi@smkn-2sbg.sch.id>
2025-12-05 14:34:35 +0700

    feat: add update task API function and axios dependency

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -6,6 +6,8 @@ export const getAllTasks = () => axios.get(API_URL);
 
 export const createTask = (taskData) => axios.post(API_URL, taskData);
 
+export const updateTask = (id, taskData) => axios.put(`${API_URL}/${id}`, taskData);
+
 export const completeTask = (id) => axios.patch(`${API_URL}/${id}/complete`);
 
 export const deleteTask = (id) => axios.delete(`${API_URL}/${id}`);
